f9eb30f4d886a5a5dff6b545f78a7abffb966bc9
Venkat Ambati <venkatrami.ambati@gmail.com>
2021-04-07 12:37:57 -0500

    Resilience4jConfig

---
+++ src/main/java/com/ttech/orch/Resilience4jConfig.java
@@ -0,0 +1,45 @@
+package com.ttech.orch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.vavr.collection.Seq;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class Resilience4jConfig {
+	
+	@Autowired
+	CircuitBreakerRegistry circuitBreakerRegistry;
+	
+	@Autowired
+	RetryRegistry retryRegistry;
+
+	@Bean
+	public Seq<CircuitBreaker> circuitBreaker(){
+		log.info("circuitBreaker");
+		//get or Create CircuitBreaker from the CircuitBreakerRegistry with custom configuration.
+		
+		Seq<CircuitBreaker> circuitBreaker = circuitBreakerRegistry.getAllCircuitBreakers();
+		log.info("{} Entering circuitBreaker() size: {}, names:{}", circuitBreaker.size(), circuitBreakerRegistry.getAllCircuitBreakers());
+		//Apply event listener for existing circuit breaker instances
+		circuitBreaker.forEach(cb -> cb.getEventPublisher().onStateTransition(event -> log.info((event.toString()))));
+		return circuitBreaker;
+	}
+	
+	@Bean
+	public Seq<Retry> retry(){
+		log.info("info");
+		Seq<Retry> retries = retryRegistry.getAllRetries();
+		log.info("Entering Retries() size:{}, name:{}",retries.size(), retryRegistry.getAllRetries() );
+		//Apply event listener for existing retry instances
+		retries.forEach(ret -> ret.getEventPublisher().onRetry(event -> log.info(event.toString())));
+		return retries;
+	}
+}
