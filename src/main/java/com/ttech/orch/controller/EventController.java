caa46b374d1f3e36b10083f6647527b9fedc7afe
Venkat Ambati <venkatrami.ambati@gmail.com>
2021-08-03 13:00:15 -0500

    gateway

--- src/main/java/com/ttech/orch/controller/EventController.java
+++ src/main/java/com/ttech/orch/controller/EventController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ttech.orch.model.Event;
 import com.ttech.orch.service.EventService;
 
 @RestController
+@RequestMapping("/event-gateway")
 public class EventController {
 	
 	@Autowired
